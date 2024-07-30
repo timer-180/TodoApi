63499aaceb4e7db1e706a2e1205dba46bff06418
timer180 <timer180@gmail.com>
2024-07-30 14:35:03 +0300

    initial

---
+++ Data/TodoApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Data
+{
+    public class TodoApiContext(DbContextOptions<TodoApiContext> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>()
+                .Property(b => b.Created)
+                .HasColumnType("datetime")
+                .HasDefaultValueSql("getdate()");
+        }
+    }
+}
