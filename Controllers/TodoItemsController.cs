06f43127ee0ca5d6343e95c7d93039a95cecdc62
timer180 <timer180@gmail.com>
2024-08-08 18:37:00 +0300

    Added repository

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -7,10 +7,16 @@ namespace TodoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TodoItemsController(ILogger<TodoItemsController> logger, TodoApiContext context) : ControllerBase
+    public class TodoItemsController : ControllerBase
     {
-        private readonly ILogger<TodoItemsController> _logger = logger;
-        private readonly TodoApiContext _context = context;
+        private readonly ILogger<TodoItemsController> _logger;
+        private readonly ITodoItemsRepository _repository;
+
+        public TodoItemsController(ILogger<TodoItemsController> logger, ITodoItemsRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
 
         // GET: TodoItems
         [HttpGet]
@@ -19,11 +25,8 @@ namespace TodoApi.Controllers
             try
             {
                 _logger.LogInformation("Request for TodoItems");
-                List<TodoItem> todoItems = await _context.TodoItems
-                    .OrderBy(item => item.Created)
-                    .ToListAsync();
+                List<TodoItem> todoItems = await _repository.GetTodoItems();
                 _logger.LogInformation("Found TodoItems: {}", todoItems);
-
                 return Ok(todoItems);
             }
             catch (Exception e)
@@ -39,16 +42,13 @@ namespace TodoApi.Controllers
             try
             {
                 _logger.LogInformation("Request for TodoItem, id={}", id);
-                var todoItem = await _context.TodoItems.FindAsync(id);
-
+                TodoItem? todoItem = await _repository.GetTodoItem(id);
                 if (todoItem == null)
                 {
-                    _logger.LogWarning("No TodoItem found, id={}", id);
                     return NotFound();
                 }
-
                 _logger.LogInformation("Found TodoItem, id={}: {}", id, todoItem);
-                return todoItem;
+                return Ok(todoItem);
             }
             catch (Exception e)
             {
@@ -63,14 +63,12 @@ namespace TodoApi.Controllers
             try
             {
                 _logger.LogInformation("Request to create TodoItem {}", todoItem);
-                _context.TodoItems.Add(todoItem);
-                await _context.SaveChangesAsync();
-                _logger.LogInformation("Created TodoItem {}", todoItem);
-
+                TodoItem createdTodoItem = await _repository.CreateTodoItem(todoItem);
+                _logger.LogInformation("Created TodoItem {}", createdTodoItem);
                 return CreatedAtAction(
                     nameof(GetTodoItem),
-                    new { id = todoItem.Id },
-                    todoItem);
+                    new { id = createdTodoItem.Id },
+                    createdTodoItem);
             }
             catch (Exception e)
             {
@@ -85,32 +83,11 @@ namespace TodoApi.Controllers
             try
             {
                 _logger.LogInformation("Request to switch TodoItem, id={}", id);
-                var todoItem = await _context.TodoItems.FindAsync(id);
+                TodoItem? todoItem = await _repository.SwitchTodoItem(id);
                 if (todoItem == null)
                 {
-                    _logger.LogWarning("No TodoItem found, id={}", id);
                     return NotFound();
                 }
-
-                todoItem.IsComplete = !todoItem.IsComplete;
-
-                try
-                {
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
-                {
-                    if (!TodoItemExists(id))
-                    {
-                        _logger.LogWarning("No TodoItem found on saving, id={}", id);
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-
                 _logger.LogInformation("Switched TodoItem to {}, id={}", todoItem.IsComplete, id);
                 return NoContent();
             }
@@ -127,16 +104,11 @@ namespace TodoApi.Controllers
             try
             {
                 _logger.LogInformation("Request to delete TodoItem, id={}", id);
-                var todoItem = await _context.TodoItems.FindAsync(id);
-                if (todoItem == null)
+                bool result = await _repository.DeleteTodoItem(id);
+                if (!result)
                 {
-                    _logger.LogWarning("No TodoItem found, id={}", id);
                     return NotFound();
                 }
-
-                _context.TodoItems.Remove(todoItem);
-                await _context.SaveChangesAsync();
-
                 _logger.LogInformation("Deleted TodoItem, id={}", id);
                 return NoContent();
             }
@@ -144,11 +116,6 @@ namespace TodoApi.Controllers
             {
                 return ErrorResult(e);
             }
-        }
-
-        private bool TodoItemExists(long id)
-        {
-            return _context.TodoItems.Any(e => e.Id == id);
         }
 
         private StatusCodeResult ErrorResult(Exception e)
